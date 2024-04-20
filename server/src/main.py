fcb45d3dbcc9c4e28404560ffdc78db1700182f0
kartemuz <kartemuz@gmail.com>
2024-04-20 22:00:56 +0300

    Changed CORS

--- server/src/main.py
+++ server/src/main.py
@@ -25,7 +25,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-                   "Authorization", "Accept", "Content-Type"]
+    allow_methods=["*"],
+    allow_headers=["*"]
 )

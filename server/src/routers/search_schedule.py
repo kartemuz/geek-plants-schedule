ad7136756e50e6ae9dec5e785b28f7739166e62c
kartemuz <kartemuz@gmail.com>
2024-04-25 02:20:39 +0300

    Changed SearchQuery

--- server/src/routers/search_schedule.py
+++ server/src/routers/search_schedule.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from server.src.database import models
 from server.src.database import session_factory
 from sqlalchemy import select, cast, String
+from sqlalchemy.orm import aliased
 
 
 search_schedule_router = APIRouter(
@@ -12,10 +13,20 @@ search_schedule_router = APIRouter(
 
 @search_schedule_router.get('/group')
 async def search_group(name: int):
-    async with session_factory() as session:
-        query = select(models.Group).filter(cast(models.Group.id, String).like(f'%{name}%'))
-        result = await session.execute(query)
-        return result.scalars().all()
+    async with (session_factory() as session):
+        group = aliased(models.Group)
+        direction = aliased(models.Direction)
+        query = select(group, direction).join(
+                direction, group.direction_id == direction.id,
+            ).filter(cast(group.id, String).like(f'%{name}%'))
+
+        query_result = await session.execute(query)
+        result = []
+        for i in query_result.fetchall():
+            g = i[0]
+            d = i[1]
+            result.append({'id': g.id, 'direction_id': g.direction_id, 'name': d.name})
+        return result
 
 
 @search_schedule_router.get('/teacher')
